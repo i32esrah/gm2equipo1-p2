fccb0a33055d787b64dc9c8151db8e22aa5ef072
Hugo Espejo <Hugo Espejo>
2025-09-22 21:57:22 +0200

    First commit

---
+++ src/main/java/com/GM2/Gm2Application.java
@@ -0,0 +1,13 @@
+package com.GM2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gm2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/GM2/Gm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GM2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

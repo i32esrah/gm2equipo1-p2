1a9438ac8aae087c5ce9a4eba94ed9debaf03785
i32esrah <i32esrah@uco.es>
2025-09-25 19:19:15 +0200

    Primer commit

--- src/main/java/com/GM2/Gm2Application.java
+++ src/main/java/com/GM2/Gm2Application.java
@@ -2,12 +2,23 @@ package com.GM2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class Gm2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Gm2Application.class, args);
 	}
+
+    @GetMapping("/")
+    public String home(){
+        return String.format("Hello world");
+    }
+
 
 }

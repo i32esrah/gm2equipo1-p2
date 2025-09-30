85a5d24988c7867799942b539655c5d3dbc00f72
i32esrah <i32esrah@uco.es>
2025-09-30 17:48:18 +0200

    Creadas las clases para la gestión de embarcaciones

---
+++ src/main/java/com/GM2/model/Embarcacion.java
@@ -0,0 +1,67 @@
+package com.GM2.model;
+
+public class Embarcacion {
+    private String matricula;
+    private String nombre;
+    private String tipo;
+    private int plazas;
+    private String dimensiones;
+    private String idPatron;
+
+    public Embarcacion(String idPatron, String dimensiones, int plazas, String tipo, String nombre, String matricula) {
+        this.idPatron = idPatron;
+        this.dimensiones = dimensiones;
+        this.plazas = plazas;
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.matricula = matricula;
+    }
+
+    public String getIdPatron() {
+        return idPatron;
+    }
+
+    public void setIdPatron(String idPatron) {
+        this.idPatron = idPatron;
+    }
+
+    public String getDimensiones() {
+        return dimensiones;
+    }
+
+    public void setDimensiones(String dimensiones) {
+        this.dimensiones = dimensiones;
+    }
+
+    public int getPlazas() {
+        return plazas;
+    }
+
+    public void setPlazas(int plazas) {
+        this.plazas = plazas;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}

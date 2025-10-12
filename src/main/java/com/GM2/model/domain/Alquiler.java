1ed9836c81bd42a13d8a05d2f99244de9046d3eb
Rafael Luque <i32lular@uco,es>
2025-10-12 12:20:00 +0200

    Constructores Alquiler y Reserva y sus tablas en base de datos

--- src/main/java/com/GM2/model/domain/Alquiler.java
+++ src/main/java/com/GM2/model/domain/Alquiler.java
@@ -1,5 +1,6 @@
 package com.GM2.model.domain;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -14,6 +15,16 @@ public class Alquiler {
     private String matricula_embarcacion;
 
 
+    public Alquiler(int id, LocalDate fechaInicio, LocalDate fechaFin, double precio, int plazas, String usuario_dni, String matricula_embarcacion, ArrayList<String> acompanantes_dni) {
+        this.id = id;
+        this.fechainicio = fechaInicio;
+        this.fechafin = fechaFin;
+        this.precio = precio;
+        this.plazas = plazas;
+        this.usuario_dni = usuario_dni;
+        this.matricula_embarcacion = matricula_embarcacion;
+        this.acompanantes_dni = acompanantes_dni;
+    }
     
     public int getId() {
         return id;

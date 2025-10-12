0948dadf93eb2ca875c882fea931dc733c4b27d1
Jose Luis <i32gacoj@uco.es>
2025-10-12 15:51:38 +0200

    Se ha completado EmbarcacionesRepository, EmbarcacionController y se han añadido los metodos necesarios a sql.properties

--- src/main/java/com/GM2/controller/EmbarcacionController.java
+++ src/main/java/com/GM2/controller/EmbarcacionController.java
@@ -1,4 +1,38 @@
 package com.GM2.controller;
 
+import com.GM2.model.domain.Embarcacion;
+import com.GM2.model.repository.EmbarcacionRepository;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/embarcaciones")
 public class EmbarcacionController {
+
+    EmbarcacionRepository embarcacionRepository;
+
+    public EmbarcacionController(EmbarcacionRepository embarcacionRepository) {
+        this.embarcacionRepository = embarcacionRepository;
+        String sqlQueriesFileName = "./src/main/resources/db/sql.properties";
+        this.embarcacionRepository.setSqlQueriesFileName(sqlQueriesFileName);
+    }
+
+    @GetMapping
+    public List<Embarcacion> getEmbarcaciones(){ return embarcacionRepository.findAllEmbarcaciones(); }
+
+    @GetMapping("/{matricula}")
+    public Embarcacion getEmbarcacionByMatricula(@PathVariable String matricula){ return embarcacionRepository.findEmbarcacionByMatricula(matricula); }
+
+    @PostMapping
+    public String addEmbarcacion(@RequestBody Embarcacion embarcacion){
+        boolean res = embarcacionRepository.addEmbarcacion(embarcacion);
+
+        if(res) {
+            return "Embarcacion was added successfully";
+        } else {
+            return "Embarcacion could not be added";
+        }
+    }
 }
--- src/main/java/com/GM2/model/repository/EmbarcacionRepository.java
+++ src/main/java/com/GM2/model/repository/EmbarcacionRepository.java
@@ -1,5 +1,115 @@
 package com.GM2.model.repository;
 
+import com.GM2.model.domain.Embarcacion;
+import com.GM2.model.domain.Reserva;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
 public class EmbarcacionRepository extends AbstractRepository {
 
+    public EmbarcacionRepository(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+
+    public List<Embarcacion> findAllEmbarcaciones() {
+        try {
+            String query = sqlQueries.getProperty("select-findAllEmbarcaciones");
+
+            if (query != null) {
+                List<Embarcacion> result = jdbcTemplate.query(query, new RowMapper<Embarcacion>() {
+                    @Override
+                    public Embarcacion mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return new Embarcacion(
+                                rs.getString("id_patron"),
+                                rs.getString("dimensiones"),
+                                rs.getInt("plazas"),
+                                rs.getString("tipo"),
+                                rs.getString("nombre"),
+                                rs.getString("matricula")
+                        );
+                    }
+                });
+
+                return result;
+            } else {
+                return null;
+            }
+
+        } catch (DataAccessException exception) {
+            System.err.println("Unable to find embarcaciones.");
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public Embarcacion findEmbarcacionByMatricula(String matricula) {
+        try {
+            String query = sqlQueries.getProperty("select-findEmbarcacionByMatricula");
+            Embarcacion result = jdbcTemplate.query(query, this::mapRowToEmbarcacion, matricula);
+            if(result != null)
+                return result;
+            else
+                return null;
+        } catch (DataAccessException exception) {
+            System.err.println("Unable to find embarcacion with matricula: " + matricula);
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    private Embarcacion mapRowToEmbarcacion(ResultSet row) {
+        try {
+
+            if(row.first()) {
+                String id_patron = row.getString("id_patron");
+                String dimensiones = row.getString("dimensiones");
+                int plazas = row.getInt("plazas");
+                String tipo = row.getString("tipo");
+                String nombre = row.getString("nombre");
+                String matricula = row.getString("matricula");
+
+                Embarcacion embarcacion = new Embarcacion(id_patron, dimensiones, plazas, tipo, nombre, matricula);
+                return embarcacion;
+            } else {
+                return null;
+            }
+
+        } catch (SQLException exception) {
+            System.err.println("Unable to retrieve results from the database");
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean addEmbarcacion(Embarcacion embarcacion) {
+        try {
+            String query = sqlQueries.getProperty("insert-addEmbarcacion");
+            if(query != null) {
+                int result = jdbcTemplate.update(query,
+                        embarcacion.getIdPatron(),
+                        embarcacion.getDimensiones(),
+                        embarcacion.getPlazas(),
+                        embarcacion.getTipo(),
+                        embarcacion.getNombre(),
+                        embarcacion.getMatricula()
+                );
+
+                if(result > 0)
+                        return true;
+                else
+                    return false;
+            } else return false;
+
+        } catch (DataAccessException exception) {
+            System.err.println("Unable to insert reservas in the database");
+        }
+
+        return false;
+    }
 }

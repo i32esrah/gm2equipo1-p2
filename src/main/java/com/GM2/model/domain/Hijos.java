7fb18c223c4f1d49c9913a0b1adbbd4f7bfb562c
Javier Costa <javier.costa.eng@gmail.com>
2025-10-16 18:28:14 +0200

    Domine, Repository and Controller de Hijos y Acompañantes

---
+++ src/main/java/com/GM2/model/domain/Hijos.java
@@ -0,0 +1,28 @@
+package com.GM2.model.domain;
+
+public class Hijos {
+    private String dni;
+    private int id_inscripcion;
+
+    public Hijos(String dni, int id_inscripcion) {
+        this.dni = dni;
+        this.id_inscripcion = id_inscripcion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getId_inscripcion() {
+        return id_inscripcion;
+    }
+
+    public void setId_inscripcion(int id_inscripcion) {
+        this.id_inscripcion = id_inscripcion;
+    }
+
+}
---
+++ src/main/java/com/GM2/model/repository/AcompañantesRepository.java
@@ -0,0 +1,88 @@
+package com.GM2.model.repository;
+
+import com.GM2.model.domain.Acompañantes;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class AcompañantesRepository extends AbstractRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public AcompañantesRepository(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+
+    public List<Acompañantes> findAllAcompañantes() {
+        try {
+            String query = sqlQueries.getProperty("select-findAllAcompañantes");
+            if(query != null){
+                List<Acompañantes> result = jdbcTemplate.query(query, new RowMapper<Acompañantes>() {
+                    public Acompañantes mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return new Acompañantes(
+                                rs.getString("dni"),
+                                rs.getInt("id_alquiler")
+                        );
+                    };
+                });
+
+                return result;
+            } else return null;
+
+        } catch (DataAccessException exception) {
+            System.err.println("Unable to find acompañantes");
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public Acompañantes findAcompañanteByDni(String dni) {
+        try {
+            String query = sqlQueries.getProperty("select-findAcompañanteByDni");
+            Acompañantes result = jdbcTemplate.query(query, this::mapRowToAcompañantes, dni);
+            if( result != null )
+                return result;
+            else return null;
+        } catch(DataAccessException exception) {
+            System.err.println("Unable to find acompañante with dni: " + dni);
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    private Acompañantes mapRowToAcompañantes(ResultSet row) {
+        try {
+
+            if(row.first()) {
+                String dni = row.getString("dni");
+                int id_alquiler = row.getInt("id_alquiler");
+
+                Acompañantes acompañante = new Acompañantes(dni, id_alquiler);
+                return acompañante;
+            } else {
+                return null;
+            }
+        } catch (SQLException exception) {
+            System.err.println("Unable to map row to Acompañantes object");
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean addAcompañante(Acompañantes acompañante) {
+        try {
+            String query = sqlQueries.getProperty("insert-addAcompañante");
+            int rowsAffected = jdbcTemplate.update(query, acompañante.getDni(), acompañante.getId_alquiler());
+            return rowsAffected > 0;
+        } catch (DataAccessException exception) {
+            System.err.println("Unable to add acompañante");
+            exception.printStackTrace();
+            return false;
+        }
+    }
+    
+}

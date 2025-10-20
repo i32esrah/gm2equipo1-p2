97353d1c72b4c53c42fe38443dbcd500241b07ba
Jose Luis <i32gacoj@uco.es>
2025-10-20 12:50:58 +0200

    esto va regular

--- src/main/java/com/GM2/controller/EmbarcacionController.java
+++ src/main/java/com/GM2/controller/EmbarcacionController.java
@@ -5,11 +5,14 @@ import com.GM2.model.domain.Patron;
 import com.GM2.model.repository.EmbarcacionRepository;
 
 import com.GM2.model.repository.PatronRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/embarcaciones")
 public class EmbarcacionController {
 
@@ -25,26 +28,59 @@ public class EmbarcacionController {
     }
 
     @GetMapping
+    @ResponseBody
     public List<Embarcacion> getEmbarcaciones(){ return embarcacionRepository.findAllEmbarcaciones(); }
 
     @GetMapping("/{matricula}")
+    @ResponseBody
     public Embarcacion getEmbarcacionByMatricula(@PathVariable String matricula){ return embarcacionRepository.findEmbarcacionByMatricula(matricula); }
 
-    @PostMapping
-    public String addEmbarcacion(@RequestBody Embarcacion embarcacion){
-        boolean res = embarcacionRepository.addEmbarcacion(embarcacion);
+//    @PostMapping()
+//    public String addEmbarcacion(@RequestBody Embarcacion embarcacion){
+//        boolean res = embarcacionRepository.addEmbarcacion(embarcacion);
+//
+//        if(res) {
+//            return "Embarcacion was added successfully";
+//        } else {
+//            return "Embarcacion could not be added";
+//        }
+//    }
 
-        if(res) {
-            return "Embarcacion was added successfully";
-        } else {
-            return "Embarcacion could not be added";
+    @GetMapping("/addEmbarcacion")
+    public ModelAndView getAddEmbarcacionView() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("addEmbarcacionView.html");
+        modelAndView.addObject("newEmbarcacion", new Embarcacion());
+        return modelAndView;
+    }
+
+    @PostMapping("/addEmbarcacion")
+    public String addEmbarcacion(@ModelAttribute Embarcacion newEmbarcacion, SessionStatus sessionStatus){
+
+        System.out.println("[EmbarcacionController] Informacion recivida: matricula=" + newEmbarcacion.getMatricula() +
+                " nombre=" + newEmbarcacion.getNombre() +
+                " tipo=" + newEmbarcacion.getTipo() +
+                " dimensiones=" + newEmbarcacion.getDimensiones() +
+                " id_patron=" + newEmbarcacion.getIdPatron() +
+                " plazas=" + newEmbarcacion.getPlazas());
+        boolean success = embarcacionRepository.addEmbarcacion(newEmbarcacion);
+        String nextPage;
+
+        if(success){
+            nextPage = "addEmbarcacionView";
         }
+        else
+            nextPage = "addEmbarcacionViewFail";
+
+        sessionStatus.setComplete();
+        return nextPage;
     }
 
     //TENGO QUE HACER AQUI LA DE OBTENER EMBARCACION POR TIPO
 
 
     @PutMapping("/{matricula}/patron")
+    @ResponseBody
     public String asociatePatronToEmbarcacion(@PathVariable String matricula, @RequestBody String patronDni){
         return assignPatronToEmbarcacion.asociatePatron(matricula, patronDni);
     }
